e8cf09e57809b216a5809f3e996662b8a062d3ba
ANOOR111 <poncesara954@gmail.com>
2025-06-23 20:02:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,20 +7,28 @@
                 const tableBody=document.querySelector('#studentTable tbody')
             
                 const scriptURL='https://script.google.com/macros/s/AKfycbzDvruQKPfGFiyT-zd-EsLpZXv-6px-mb3-cum_P993zIoL6-F6tmvdIbRgJRZ-nk5u5Q/exec'
-    
+               const submittedAdms = new Set();
+
         submit.addEventListener("click", (e) => {
   e.preventDefault();
-
             let name = student_name.value.trim().toLowerCase();
             let admNo = String(adm.value);
             let department = student_dep.value.trim().toLowerCase();
             let level = Number(student_level.value);
             let study = Number(year_study.value);
 
+      if (submittedAdms.has(admNo)) {
+  alert(`⚠️ Admission No. ${admNo} has already submitted!`);
+  return;
+}
+
         const check = confirm("Are you sure you want to submit!!");
         const match=students.find(s=>s.studentName===name && s.adm===admNo);
+        
+        
 
   if (match && department==="ict" && level===300 && study===5) {
+           submittedAdms.add(admNo);
              document.getElementById('prom').innerHTML = `✅✅  ${name.toUpperCase()} you have submitted successfully`;
 
     const data = {
@@ -40,10 +48,15 @@
 
     
    
-  } else {
+  } 
+   
+
+  else {
         alert(`${name} with Admission No. ${admNo} not found in the record!`);
 
   }
+
+   
 
   setTimeout(() => {
     document.getElementById('prom').innerHTML = "";
